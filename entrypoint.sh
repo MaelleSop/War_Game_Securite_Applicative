c2e6716aae1115efcf40f9855f7e41d295fe8d1d
MaelleSop <maelle.sopransi@isen-ouest.yncrcrea.fr>
2025-10-17 12:50:13 +0200

    Fix 2 docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Donner les droits à Apache pour le dossier uploads
+chown -R www-data:www-data /var/www/html/uploads
+chmod -R 775 /var/www/html/uploads
+
 # Initialiser la base de données si elle n'existe pas
 if [ ! -f /var/www/html/uploads/database.sqlite ]; then
     echo "Initialisation de la base de données..."
