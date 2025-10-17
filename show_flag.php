ed273b09d50bcfa0e6b03d839a2124e792857218
MaelleSop <maelle.sopransi@isen-ouest.yncrcrea.fr>
2025-10-17 12:09:48 +0200

    Fix SSRF

---
+++ show_flag.php
@@ -0,0 +1,16 @@
+<?php
+$file = __DIR__ . '/proteger/.passwd';
+
+if (!is_readable($file)) {
+    http_response_code(404);
+    exit('Fichier introuvable');
+}
+
+// Pour forcer l'encodage pour le navigateur
+header('Content-Type: text/plain; charset=utf-8');
+header('Content-Disposition: inline; filename=".passwd"');
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+
+readfile($file);
+exit;
